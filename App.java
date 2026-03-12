3222517bfd9600d4db61e74cecd1693cfc1ee8ce
Tsukasa <nlong3242@gmail.com>
2026-03-13 00:41:43 +1300

    Add createWorkout method that let user create a workout and add exercise to it

--- App.java
+++ App.java
@@ -1,26 +1,73 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
     Scanner scanner = new Scanner(System.in);
+    ArrayList<Workout> workouts = new ArrayList<>();
+
+    void createWorkout(){
+        boolean running = true;
+        System.out.print("Enter workout name: ");
+        scanner.nextLine();
+        String name = scanner.nextLine();
+        int input = 0;
+
+        Workout workout = new Workout(name);
+        workouts.add(workout);
+        while (running) {
+            System.out.println("1. Add new exercise");
+            System.out.println("2. Back to menu");
+            System.out.print("Choose an option: ");
+            try {
+                input = scanner.nextInt();
+            } catch (Exception e) {
+                System.out.println("Invalid choice, please enter a number");
+                scanner.nextLine();
+                continue;
+            }
+
+            scanner.nextLine(); 
+            if (input == 1){
+                System.out.print("Enter the name of your exercise: ");
+                String exerciseName = scanner.nextLine();
+                Exercise exercise = new Exercise(exerciseName);
+                workout.addExercise(exercise);
+            }
+            else if (input == 2)
+                running = false;
+            else
+                System.out.println("Please choose option from menu");
+        }
+    }
 
     void run(){
         boolean running = true;
-        Scanner scanner = new Scanner(System.in);
+        int input = 0;
+        System.out.println("Welcome to Tsu2Track!");
         while (running) { 
             System.out.println("1. Create Workout");
             System.out.println("2. View Workout");
             System.out.println("3. Quit");
             System.out.print("Choose an option: ");
 
+            // Handling invalid inputs
             try {
-                int input = scanner.nextInt();
-                if (input == 3)
-                    running = false;
+                input = scanner.nextInt();
             } catch (Exception e) {
                 System.out.println("Invalid choice, please enter a number");
                 scanner.nextLine();
+                continue;
             }
 
+            //Handling inputs 
+            if (input == 3)
+                running = false;
+
+            else if (input == 1)
+                createWorkout();
+            
+            else
+                System.out.println("Please choose option from menu");
         }
     }
 }

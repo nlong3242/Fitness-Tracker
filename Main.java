111f839ee05d441efaafa2d70b4c88c098f415ab
nlong3242 <nlong3242@gmail.com>
2026-03-12 19:02:09 +1300

    Testing Workout objec

--- Main.java
+++ Main.java
@@ -12,6 +12,6 @@ public class Main {
         workoutA.addExercise(bench);
         workoutA.addExercise(pulldown);
         
-        System.out.println(workoutA);
+        System.out.println(workoutA.exercises.get(1));
     }
 }

0c55b0d3cad30d8f90984419495829cc9348b140
Tsukasa <nlong3242@gmail.com>
2026-04-07 22:43:55 +1200

    Start with implementing API calls

---
+++ backend/fitness-tracker/src/test/java/com/tsu2track/fitness_tracker/FitnessTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsu2track.fitness_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

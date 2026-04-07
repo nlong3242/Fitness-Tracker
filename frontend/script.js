bb16906144ffe31b73a2605d3ece25328bd2e8c5
Tsukasa <nlong3242@gmail.com>
2026-04-07 19:36:38 +1200

    Added addExercise method and update viewWorkout to print out list of exercies for that workout

--- frontend/script.js
+++ frontend/script.js
@@ -3,7 +3,7 @@ let workouts = [];
 function createWorkout() {
     let name = prompt("Enter workout name:");
     if (name) {
-        workouts.push(name);
+        workouts.push({name: name, exercises: []});
         renderWorkouts();
     }
 }
@@ -19,7 +19,7 @@ function renderWorkouts() {
     let list = document.getElementById("workout-list");
     for (let i = 0; i < workouts.length; i++) {
         let item = document.createElement("li");
-        let nameText = document.createTextNode(workouts[i]);
+        let nameText = document.createTextNode(workouts[i].name);
         item.style.cursor = "pointer";
         item.onclick = function() {
             viewWorkout(i);
@@ -40,15 +40,40 @@ function renderWorkouts() {
 }
 
 function viewWorkout(index) {
-    let name = workouts[index];
+    let workout = workouts[index];
     let container = document.getElementById("container");
     container.innerHTML = `
-        <h2>${name}</h2>
-        <button onclick="addExercise()">Add Exercise</button>
+        <h2>${workout.name}</h2>
+        <button onclick="addExercise(${index})">Add Exercise</button>
         <ul id="exercise-list">
         </ul>
         <button onclick="renderWorkouts()">Back</button>
     `;
+
+    let list = document.getElementById("exercise-list");
+    for (let i = 0 ; i < workout.exercises.length; i++) {
+        let item = document.createElement("li");
+        let nameText = document.createTextNode(workout.exercises[i]);
+
+        let deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "Delete";
+        deleteBtn.onclick = function() {
+            workout.exercises.splice(i, 1);
+            viewWorkout(index);
+        };
+
+        item.appendChild(nameText);
+        item.appendChild(deleteBtn);
+        list.appendChild(item);
+    }
+}
+
+function addExercise(workoutIndex) {
+    let name = prompt("Enter exercise name:")
+    if (name) {
+        workouts[workoutIndex].exercises.push(name);
+        viewWorkout(workoutIndex);
+    }
 }
 
 renderWorkouts()
